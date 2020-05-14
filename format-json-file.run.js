9a2bfbf0db3c7a1f597b81d07413a0cc57e528cc
Richeve S. Bebebdor <richeve.bebedor@gmail.com>
2020-05-14 21:53:11 +0800

    Added support for sortProperty and propertyList parameter.

--- format-json-file.run.js
+++ format-json-file.run.js
@@ -4,6 +4,14 @@ const FILE_PATH_SHELL_PARAMETER = (
 	"--filePath"
 );
 
+const SORT_PROPERTY_SHELL_PARAMETER = (
+	"--sortProperty"
+);
+
+const PROPERTY_LIST_SHELL_PARAMETER = (
+	"--propertyList"
+);
+
 (
 	async	function runFormatJSONFile( shellParameterList ){
 				"use strict";
@@ -12,15 +20,43 @@ const FILE_PATH_SHELL_PARAMETER = (
 
 				return	(
 							await	formatJSONFile(
-										shellParameterList[
-											(
-													shellParameterList
-													.indexOf(
-														FILE_PATH_SHELL_PARAMETER
+										(
+											shellParameterList[
+												(
+														shellParameterList
+														.indexOf(
+															FILE_PATH_SHELL_PARAMETER
+														)
+													+	1
+												)
+											]
+										),
+
+										(
+											{
+												"sortProperty": (
+														shellParameterList
+														.includes(
+															SORT_PROPERTY_SHELL_PARAMETER
+														)
+													===	true
+												),
+
+												"propertyList": (
+													(
+															shellParameterList[
+																	shellParameterList
+																	.indexOf(
+																		PROPERTY_LIST_SHELL_PARAMETER
+																	)
+																+	1
+															]
+														||	[ ]
 													)
-												+	1
-											)
-										]
+													.split( "," )
+												)
+											}
+										)
 									)
 						);
 			}
