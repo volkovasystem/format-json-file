32aeb58a6a24123ce1e960d800c8456387662b82
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-07-06 03:46:55 +0800

    Added fallback if filePath shell parameter is optional.
    Formatted code.

--- format-json-file.run.js
+++ format-json-file.run.js
@@ -12,52 +12,134 @@ const PROPERTY_LIST_SHELL_PARAMETER = (
 	"--propertyList"
 );
 
+const JSON_FILE_EXTENSION_PATTERN = (
+	/\.json$/
+);
+
 (
 	async	function runFormatJSONFile( shellParameterList ){
 				"use strict";
 
-				const formatJSONFile = require( "./format-json-file.js" );
+				const formatJSONFile = (
+					require( "./format-json-file.js" )
+				);
+
+				const filePath = (
+						(
+								(
+										shellParameterList
+										.includes(
+											(
+												FILE_PATH_SHELL_PARAMETER
+											)
+										)
+									===	true
+								)
+						)
+					?	(
+							shellParameterList[
+								(
+									(
+										shellParameterList
+										.indexOf(
+											(
+												FILE_PATH_SHELL_PARAMETER
+											)
+										)
+									)++
+								)
+							]
+						)
+					:	(
+							shellParameterList
+							.find(
+								(
+									( shellParameter ) => (
+										JSON_FILE_EXTENSION_PATTERN
+										.test(
+											(
+												shellParameter
+											)
+										)
+									)
+								)
+							)
+						)
+				);
+
+				const sortProperty = (
+						(
+								shellParameterList
+								.includes(
+									(
+										SORT_PROPERTY_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+				);
+
+				const propertyList = (
+						(
+								(
+										shellParameterList
+										.includes(
+											(
+												PROPERTY_LIST_SHELL_PARAMETER
+											)
+										)
+									===	true
+								)
+						)
+					?	(
+							(
+								shellParameterList[
+									(
+										(
+											shellParameterList
+											.indexOf(
+												(
+													PROPERTY_LIST_SHELL_PARAMETER
+												)
+											)
+										)++
+									)
+								]
+							)
+							.split(
+								(
+									","
+								)
+							)
+						)
+					:	(
+							[ ]
+						)
+				);
 
 				return	(
 							await	formatJSONFile(
 										(
-											shellParameterList[
-												(
-														shellParameterList
-														.indexOf(
-															FILE_PATH_SHELL_PARAMETER
-														)
-													+	1
-												)
-											]
+											filePath
 										),
 
 										(
 											{
 												"sortProperty": (
-														shellParameterList
-														.includes(
-															SORT_PROPERTY_SHELL_PARAMETER
-														)
-													===	true
+													sortProperty
 												),
 
 												"propertyList": (
-													(
-															shellParameterList[
-																	shellParameterList
-																	.indexOf(
-																		PROPERTY_LIST_SHELL_PARAMETER
-																	)
-																+	1
-															]
-														||	[ ]
-													)
-													.split( "," )
+													propertyList
 												)
 											}
 										)
 									)
 						);
 			}
-)( process.argv );
+)(
+	(
+		process
+		.argv
+	)
+);
