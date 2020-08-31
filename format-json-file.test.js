0dd78d808b71c6e8b503052637ccac0af4f80b91
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2020-08-31 15:06:15 +0800

    Formatted code.

--- format-json-file.test.js
+++ format-json-file.test.js
@@ -1,11 +1,366 @@
 "use strict";
 
+//; @start:code-space:template-engine;
 const assert = require( "assert" );
+const util = require( "util" );
 
 const strictAssert = (
 	assert
 	.strict
 );
+
+//; @start:procedure:check-directory;
+const checkDirectory = (
+	async	function checkDirectory( directoryPath ){
+					const fs = require( "fs" );
+					const path = require( "path" );
+
+					const fsAsync = (
+						fs
+						.promises
+					);
+
+						directoryPath
+					=	(
+							path
+							.resolve(
+								(
+									directoryPath
+								)
+							)
+						);
+
+					try{
+						return	(
+									(
+										await	fsAsync
+												.stat(
+													(
+														directoryPath
+													)
+												)
+									)
+									.isDirectory( )
+								);
+					}
+					catch( error ){
+						return	(
+									false
+								);
+					}
+			}
+);
+//; @end:procedure:check-directory;
+
+//; @start:procedure:check-file;
+const checkFile = (
+	async	function checkFile( filePath ){
+					const fs = require( "fs" );
+					const path = require( "path" );
+
+					const fsAsync = (
+						fs
+						.promises
+					);
+
+						filePath
+					=	(
+							path
+							.resolve(
+								(
+									filePath
+								)
+							)
+						);
+
+					try{
+						return	(
+									(
+										await	fsAsync
+												.stat(
+													(
+														filePath
+													)
+												)
+									)
+									.isFile( )
+								);
+					}
+					catch( error ){
+						return	(
+									false
+								);
+					}
+			}
+);
+//; @end:procedure:check-file;
+
+//;	@start:procedure:get-directory-file-list;
+const getDirectoryFileList = (
+	async	function getDirectoryFileList( directoryPath ){
+				const fs = require( "fs" );
+				const path = require( "path" );
+
+				const fsAsync = (
+					fs
+					.promises
+				);
+
+					directoryPath
+				=	(
+						path
+						.resolve(
+							(
+								directoryPath
+							)
+						)
+					);
+
+				try{
+					return	(
+								await	fsAsync
+										.readdir(
+											(
+												directoryPath
+											),
+
+											(
+												{
+													"withFileTypes": (
+														true
+													)
+												}
+											)
+										)
+							);
+				}
+				catch( error ){
+					return	(
+								[ ]
+							);
+				}
+			}
+);
+//; @end:procedure:get-directory-file-list;
+
+//;	@start:procedure:execute-shell-script;
+const executeShellScript = (
+	async	function executeShellScript( shellScript, moduleDirectoryPath ){
+				const childProcess = require( "child_process" );
+				const path = require( "path" );
+
+				const execAsync = (
+					util
+					.promisify(
+						(
+							childProcess
+							.exec
+						)
+					)
+				);
+
+				if(
+						(
+								typeof
+								moduleDirectoryPath
+							==	"string"
+						)
+
+					&&	(
+								moduleDirectoryPath
+								.length
+							>	0
+						)
+				){
+					moduleDirectoryPath = (
+						path
+						.resolve(
+							(
+								moduleDirectoryPath
+							)
+						)
+					);
+				}
+				else{
+					moduleDirectoryPath = (
+						process
+						.cwd( )
+					);
+				}
+
+				try{
+					const	{
+								stdout,
+								stderr
+							}
+						=	(
+								await	execAsync(
+											(
+												shellScript
+											),
+
+											(
+												{
+													"cwd": (
+														moduleDirectoryPath
+													)
+												}
+											)
+										)
+							);
+
+					return	(
+								{
+									"outputLog": (
+										stdout
+										.trim( )
+									),
+
+									"errorLog": (
+										stderr
+										.trim( )
+									)
+								}
+							);
+				}
+				catch( error ){
+					return	(
+								{
+									"error": (
+										util
+										.inspect(
+											(
+												error
+											)
+										)
+									)
+								}
+							);
+				}
+			}
+);
+//;	@end:procedure:execute-shell-script;
+
+//;	@start:procedure:setup-test-directory;
+const SETUP_TEST_DIRECTORY = (
+	async	function SETUP_TEST_DIRECTORY( ){
+				const shellParameterList = (
+					process
+					.argv
+				);
+
+				const DISABLE_SETUP_TEST_DIRECTORY_SHELL_PARAMETER = (
+					"--disableSetupTestDirectory"
+				);
+
+				const DISABLE_SETUP_TEST_DIRECTORY_SHORT_SHELL_PARAMETER = (
+					"--xstd"
+				);
+
+				const disableSetupTestDirectory = (
+						(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_SETUP_TEST_DIRECTORY_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+
+					||	(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_SETUP_TEST_DIRECTORY_SHORT_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+				);
+
+				if(
+						(
+								disableSetupTestDirectory
+							===	true
+						)
+				){
+					return	(
+								true
+							);
+				}
+
+				return	(
+							await	executeShellScript(
+										(
+											"mkdir .test || true"
+										)
+									)
+						);
+			}
+);
+//;	@end:procedure:setup-test-directory;
+
+//;	@start:procedure:clean-test-directory;
+const CLEAN_TEST_DIRECTORY = (
+	async	function CLEAN_TEST_DIRECTORY( ){
+				const shellParameterList = (
+					process
+					.argv
+				);
+
+				const DISABLE_CLEAN_TEST_DIRECTORY_SHELL_PARAMETER = (
+					"--disableCleanTestDirectory"
+				);
+
+				const DISABLE_CLEAN_TEST_DIRECTORY_SHORT_SHELL_PARAMETER = (
+					"--xctd"
+				);
+
+				const disableCleanTestDirectory = (
+						(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_CLEAN_TEST_DIRECTORY_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+
+					||	(
+								shellParameterList
+								.includes(
+									(
+										DISABLE_CLEAN_TEST_DIRECTORY_SHORT_SHELL_PARAMETER
+									)
+								)
+							===	true
+						)
+				);
+
+				if(
+						(
+								disableCleanTestDirectory
+							===	true
+						)
+				){
+					return	(
+								true
+							);
+				}
+
+				return	(
+							await	executeShellScript(
+										(
+											"rm -rfv .test || true"
+										)
+									)
+						);
+			}
+);
+//;	@end:procedure:clean-test-directory;
+//; @end:code-space:template-engine;
 
 const formatJSONFile = (
 	require( "./format-json-file.js" )
@@ -14,6 +369,14 @@ const formatJSONFile = (
 const TEST_PACKAGE_JSON_FILE = (
 	async	function TEST_PACKAGE_JSON_FILE( ){
 				try{
+					const actualValue = (
+						await	formatJSONFile(
+									(
+										"./package.json"
+									)
+								)
+					);
+
 					const testValue = (
 						true
 					);
@@ -21,11 +384,7 @@ const TEST_PACKAGE_JSON_FILE = (
 					strictAssert
 					.equal(
 						(
-							await	formatJSONFile(
-										(
-											"./package.json"
-										)
-									)
+							actualValue
 						),
 
 						(
@@ -37,7 +396,7 @@ const TEST_PACKAGE_JSON_FILE = (
 								"#test-package-json-file;",
 
 								"test package json file;",
-								`must return ${ testValue };`
+								`must return, ${ testValue };`
 							]
 						)
 					);
@@ -64,6 +423,14 @@ const TEST_PACKAGE_JSON_FILE = (
 const TEST_PACKAGE_LOCK_JSON_FILE = (
 	async	function TEST_PACKAGE_LOCK_JSON_FILE( ){
 				try{
+					const actualValue = (
+						await	formatJSONFile(
+									(
+										"./package-lock.json"
+									)
+								)
+					);
+
 					const testValue = (
 						true
 					);
@@ -71,11 +438,7 @@ const TEST_PACKAGE_LOCK_JSON_FILE = (
 					strictAssert
 					.equal(
 						(
-							await	formatJSONFile(
-										(
-											"./package-lock.json"
-										)
-									)
+							actualValue
 						),
 
 						(
@@ -87,7 +450,7 @@ const TEST_PACKAGE_LOCK_JSON_FILE = (
 								"#test-package-lock-json-file;",
 
 								"test package lock json file;",
-								`must return ${ testValue };`
+								`must return, ${ testValue };`
 							]
 						)
 					);
@@ -113,6 +476,10 @@ const TEST_PACKAGE_LOCK_JSON_FILE = (
 
 (
 	async	function TEST_SCENE_BASIC( ){
+				(
+					await	CLEAN_TEST_DIRECTORY( )
+				);
+
 				console
 				.table(
 					(
@@ -138,6 +505,10 @@ const TEST_PACKAGE_LOCK_JSON_FILE = (
 							}
 						]
 					)
+				);
+
+				(
+					await	CLEAN_TEST_DIRECTORY( )
 				);
 			}
 )( );
