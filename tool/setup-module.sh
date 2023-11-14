8ebad5a4eac42e1432dc1fd40746fd5746903b30
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2023-11-15 07:29:54 +0800

    Improved build procedure.

--- tool/setup-module.sh
+++ tool/setup-module.sh
@@ -3,15 +3,21 @@
 [[ -f "$MODULE_SETUP_LIST_FILE_PATH" ]] &&	\
 cat "$MODULE_SETUP_LIST_FILE_PATH" | xargs -n1 -I {} bash -c "{}";
 
-[[ -f "$MODULE_DEPENDENCY_LIST_FILE_PATH" ]] &&	\
-[[ -f "$MODULE_PACKAGE_FILE_PATH" ]] &&			\
+[[ -f "$MODULE_ROOT_DIRECTORY_PATH/setup.lock" ]] &&	\
+[[ -f "$MODULE_DEPENDENCY_LIST_FILE_PATH" ]] &&			\
+[[ -f "$MODULE_PACKAGE_FILE_PATH" ]] &&					\
 cp "$MODULE_PACKAGE_FILE_PATH" "$MODULE_PACKAGE_FILE_PATH.backup";
 
 install-dependency( ){
 	local MODULE=$1;
 
-	[[ ! -z "$MODULE" ]] &&	\
+	[[ -f "$MODULE_ROOT_DIRECTORY_PATH/setup.lock" ]] &&	\
+	[[ ! -z "$MODULE" ]] &&									\
 	npm install "$MODULE" --no-save --force;
+
+	[[ ! -f "$MODULE_ROOT_DIRECTORY_PATH/setup.lock" ]] &&	\
+	[[ ! -z "$MODULE" ]] &&									\
+	npm install "$MODULE" --save-exact --force;
 };
 
 export -f install-dependency;
@@ -19,11 +25,15 @@ export -f install-dependency;
 [[ -f "$MODULE_DEPENDENCY_LIST_FILE_PATH" ]] &&	\
 cat "$MODULE_DEPENDENCY_LIST_FILE_PATH" | xargs -n1 -I {} bash -c "install-dependency {}";
 
-[[ -f "$MODULE_PACKAGE_FILE_PATH.backup" ]] &&	\
+[[ -f "$MODULE_ROOT_DIRECTORY_PATH/setup.lock" ]] &&	\
+[[ -f "$MODULE_PACKAGE_FILE_PATH.backup" ]] &&			\
 mv --force "$MODULE_PACKAGE_FILE_PATH.backup" "$MODULE_PACKAGE_FILE_PATH";
 
 [[ -d "$TRASH_DIRECTORY" ]] &&					\
 [[ -f "$MODULE_PACKAGE_LOCK_FILE_PATH" ]] &&	\
 mv --force "$MODULE_PACKAGE_LOCK_FILE_PATH" "$TRASH_DIRECTORY";
+
+[[ ! -f "$MODULE_ROOT_DIRECTORY_PATH/setup.lock" ]] && \
+touch "$MODULE_ROOT_DIRECTORY_PATH/setup.lock";
 
 return 0;
